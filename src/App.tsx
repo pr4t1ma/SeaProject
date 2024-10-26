7aae013df9650b59e5b3fa0586aee4ac8a6f442e
Pam <prati.ma@hotmail.com>
2024-10-26 10:11:34 +0200

    All changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+
 const items = [
   { label: "Home", href: "./" },
   { label: "About", href: "./about" },
@@ -9,9 +10,26 @@ const items = [
   { label: "Contact", href: "./contact" },
   { label: "FAQ", href: "./faq" },
 ];
+type Pokemon = {
+  name: string;
+  type: string;
+};
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+  const getPokemonData = async () => {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon/").then(
+      (res) => res.json()
+    );
+    const data = response.results;
+    setPokemons(data);
+  };
+
+  useEffect(() => {
+    getPokemonData();
+  }, []);
   return (
     <div className="container mx-auto my-20">
       <h1 className="text-red-700 text-3xl">Here will be the main heading</h1>
@@ -44,6 +62,14 @@ function App() {
             </button>
           )}
         </nav>
+      </div>
+      <div>
+        <h4>Pokemon list</h4>
+        <ul>
+          {pokemons.map((pokemon) => {
+            return <li key={pokemon.name}>{pokemon.name}</li>;
+          })}
+        </ul>
       </div>
     </div>
   );
